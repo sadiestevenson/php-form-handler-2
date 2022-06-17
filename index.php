85ce14eeda0c413569bdbbe47ff2f312ad36c3eb
Sadie Stevenson <93157504+sadiestevenson@users.noreply.github.com>
2022-06-17 21:20:06 +0000

    added a small form handler

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <html>
   <head>
-    <title>PHP Test Page</title>
+    <title>World's Smallest Form Handler</title>
   </head>
   <body>
-    <h1>PHP Test Page </h1>
-    <?php
-    echo '<p>This is PHP!</p>';
-    ?>
-    <p>Herllo From Github</p>
-      <?php
-    echo '<p>This is a change from replit!</p>';
-    ?>
-    <p>My VSCODE is giving me a fatl error message</p>
+    <h1>World's Smallest Form Handler </h1>
+
+<?php
+if(isset($_POST["FirstName"])){//show data
+  echo $_POST["FirstName"];
+}else{//show form
+  echo '
+       <form method="post" action="">
+       <p> First Name: <input type="text"         name="FirstName"/></p>
+       <p><input type="Submit" </p>
+       </form>
+       ';
+} 
+
+
+?>
+
+
+
+    
+
+
   </body>
 </html>
